52c15343e1c0a8ef7ec3470328749dce144f1aa4
almazfarhatovich <janybekovalmaz29@gmail.com>
2022-05-29 15:42:23 +0600

    ff

--- src/main/java/almaz/controllers/FirstController.java
+++ src/main/java/almaz/controllers/FirstController.java
@@ -1,8 +1,12 @@
 package almaz.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class FirstController {
@@ -14,7 +18,32 @@ public class FirstController {
     }
 
     @GetMapping("/hello")
-    public String hello() {
+    public String hello(@RequestParam(value = "name", required = false) String name, @RequestParam(value = "surname", required = false) String surname,
+                        Model model) {
+        model.addAttribute("message", "hello, " + name + " " + surname);
         return "first1/hello";
+    }
+
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam("a") int a, @RequestParam("b") int b, @RequestParam("action") String action,
+                             Model model
+    ) {
+        double result = 0;
+        switch (action) {
+            case "multiplication":
+                result = a * b;
+                break;
+            case "division":
+                result = a / (double)b;
+                break;
+            case "subtraction":
+                result = a - b;
+                break;
+            case "addition":
+                result = a + b;
+                break;
+        }
+        model.addAttribute("result",result);
+        return "first1/calculator";
     }
 }
